8ca76727fd83d894d357b97b12121fd00bc1e790
Luis Ginés Casanova <luisginesc@gmail.com>
2025-09-29 15:13:18 +0200

    Delete Book management dinamic application (Spanish) directory

--- Book management dinamic application (Spanish)/backend.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__.'/setup.php';
-
-session_start();
-
-if($jaxon->canProcessRequest())
-{
-    $jaxon->processRequest();
-}
--- Book management dinamic application (Spanish)/comun.php
+++
@@ -1,6 +0,0 @@
-<?php
-error_reporting(E_ALL ^ E_DEPRECATED);
-
-require_once __DIR__.'/vendor/autoload.php';
-
-define('BASE_URL','http://'.$_SERVER['HTTP_HOST'].str_replace(basename($_SERVER['PHP_SELF']),'/',explode('?',$_SERVER['REQUEST_URI'])[0]).'/');
